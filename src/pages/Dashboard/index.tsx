8fa43ea92105c180da9fffee891ab187e56e1b0c
Guilherme Lopes <guilhermelopes.silva@hotmail.com>
2021-05-05 22:09:10 -0300

    dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/15221610?v=4"
+            alt="Guilherme Lopes"
+          />
+          <div>
+            <strong>lopes-guilhermes/curso-node-caelum</strong>
+            <p>Curso NodeJS e Express- Caelum</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/15221610?v=4"
+            alt="Guilherme Lopes"
+          />
+          <div>
+            <strong>lopes-guilhermes/curso-node-caelum</strong>
+            <p>Curso NodeJS e Express- Caelum</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/15221610?v=4"
+            alt="Guilherme Lopes"
+          />
+          <div>
+            <strong>lopes-guilhermes/curso-node-caelum</strong>
+            <p>Curso NodeJS e Express- Caelum</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
